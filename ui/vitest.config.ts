44441b32012d1d6a827edfd4867ff20b8d7ae790
linghaoSu <linghao.su@daocloud.io>
2026-02-27 17:34:05 +0800

    chore: support test utils

---
+++ ui/vitest.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    include: ['src/**/*.test.{ts,tsx}'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'src/test/'],
+    },
+  },
+})

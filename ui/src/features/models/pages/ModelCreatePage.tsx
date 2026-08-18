5f4c1af5a46dcc82791e32adc5d009b30d1f38b9
linghaoSu <linghao.su@daocloud.io>
2026-08-18 17:27:06 +0800

    fix(ui): create projects from model form
    Signed-off-by: linghaoSu <linghao.su@daocloud.io>

--- ui/src/features/models/pages/ModelCreatePage.tsx
+++ ui/src/features/models/pages/ModelCreatePage.tsx
@@ -1,25 +1,29 @@
 import {
-  Anchor,
   Alert,
   Box,
   Button,
   Group,
+  Paper,
   rem,
   Stack,
   Text,
   TextInput,
 } from '@mantine/core'
-import { IconExternalLink, IconInfoCircle } from '@tabler/icons-react'
-import { useMutation } from '@tanstack/react-query'
+import { useDisclosure } from '@mantine/hooks'
 import {
-  Link, useNavigate, useRouter,
-} from '@tanstack/react-router'
+  IconFolderPlus,
+  IconInfoCircle,
+  IconPlus,
+} from '@tabler/icons-react'
+import { useMutation } from '@tanstack/react-query'
+import { useNavigate, useRouter } from '@tanstack/react-router'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { createModelMutationOptions } from '@/features/models/models.mutation'
 import { useWritableModelProjects } from '@/features/models/models.query.ts'
 import { createModelSchema } from '@/features/models/models.schema'
+import { CreateProjectModal } from '@/features/projects/components/CreateProjectModal'
 import { ProjectSelect } from '@/shared/components/ProjectSelect'
 import { useForm } from '@/shared/hooks/useForm'
 import { fieldError } from '@/shared/utils/form.ts'
@@ -32,6 +36,7 @@ export function ModelCreatePage({ initialProjectId = '' }: ModelCreatePageProps)
   const { t } = useTranslation()
   const navigate = useNavigate()
   const router = useRouter()
+  const [createProjectOpened, createProjectHandlers] = useDisclosure(false)
 
   const mutation = useMutation(createModelMutationOptions())
   const modelCreateSchema = createModelSchema(t)
@@ -71,7 +76,21 @@ export function ModelCreatePage({ initialProjectId = '' }: ModelCreatePageProps)
     },
   })
 
-  const { data: projects = [] } = useWritableModelProjects()
+  const {
+    data: projects = [],
+    isLoadingError: isProjectsLoadingError,
+    isPending: isProjectsPending,
+    refetch: refetchProjects,
+  } = useWritableModelProjects()
+  const showEmptyProjectPrompt = !initialProjectId
+    && !isProjectsPending
+    && !isProjectsLoadingError
+    && projects.length === 0
+
+  const handleProjectCreated = async (projectName: string) => {
+    await refetchProjects()
+    form.setFieldValue('projectId', projectName)
+  }
 
   useEffect(() => {
     const projectId = form.state.values.projectId
@@ -121,42 +140,79 @@ export function ModelCreatePage({ initialProjectId = '' }: ModelCreatePageProps)
             name="projectId"
             validators={{ onChange: projectIdValidator }}
           >
-            {field => (
-              <Group align="flex-end" wrap="nowrap">
-                <Box flex={1}>
-                  <ProjectSelect
-                    data={projects}
-                    value={field.state.value}
-                    onChange={field.handleChange}
-                    inputProps={{
-                      disabled: !!initialProjectId,
-                      onBlur: field.handleBlur,
-                      error: fieldError(field),
-                    }}
-                  />
-                </Box>
-                {!initialProjectId && (
-                  <Anchor
-                    component={Link}
-                    to="/projects"
-                    pb="xs"
-                    size="md"
-                    c="var(--mantine-color-text)"
-                    underline="never"
-                    flex="0 0 auto"
-                  >
-                    <Group gap={6} wrap="nowrap">
-                      {t('model.create.createProject')}
-                      <IconExternalLink
-                        size={28}
-                        stroke={1.75}
+            {(field) => {
+              const error = fieldError(field)
+
+              if (showEmptyProjectPrompt) {
+                return (
+                  <Paper withBorder radius="md" px="lg" py="xl">
+                    <Stack align="center" gap="xs">
+                      <IconFolderPlus
+                        size={44}
+                        stroke={1.25}
                         color="var(--mantine-primary-color-filled)"
                       />
-                    </Group>
-                  </Anchor>
-                )}
-              </Group>
-            )}
+                      <Text fw={600}>{t('model.create.emptyProjectTitle')}</Text>
+                      <Text size="sm" c="dimmed" ta="center">
+                        {t('model.create.emptyProjectDescription')}
+                      </Text>
+                      <Button
+                        type="button"
+                        mt="xs"
+                        onClick={createProjectHandlers.open}
+                      >
+                        {t('model.create.createProject')}
+                      </Button>
+                    </Stack>
+                  </Paper>
+                )
+              }
+
+              return (
+                <Group align="flex-start" gap="sm" wrap="nowrap">
+                  <Box flex="0 0 100%">
+                    <ProjectSelect
+                      data={projects}
+                      value={field.state.value}
+                      onChange={field.handleChange}
+                      inputProps={{
+                        disabled: !!initialProjectId || isProjectsPending,
+                        onBlur: field.handleBlur,
+                        error,
+                        styles: {
+                          input: error
+                            ? {
+                                borderColor: 'var(--mantine-color-red-4)',
+                                boxShadow: '0 0 0 2px var(--mantine-color-red-0)',
+                              }
+                            : undefined,
+                          error: {
+                            color: 'var(--mantine-color-red-6)',
+                            fontWeight: 500,
+                            lineHeight: rem(16),
+                            marginTop: rem(5),
+                          },
+                        },
+                      }}
+                    />
+                  </Box>
+                  {!initialProjectId && (
+                    <Button
+                      type="button"
+                      variant="subtle"
+                      mt={rem(29)}
+                      h={36}
+                      flex="0 0 auto"
+                      px="xs"
+                      leftSection={<IconPlus size={16} />}
+                      onClick={createProjectHandlers.open}
+                    >
+                      {t('model.create.createProject')}
+                    </Button>
+                  )}
+                </Group>
+              )
+            }}
           </form.Field>
 
           <Alert
@@ -192,6 +248,12 @@ export function ModelCreatePage({ initialProjectId = '' }: ModelCreatePageProps)
           </form.Subscribe>
         </Stack>
       </form>
+
+      <CreateProjectModal
+        opened={createProjectOpened}
+        onClose={createProjectHandlers.close}
+        onCreated={handleProjectCreated}
+      />
     </Stack>
   )
 }
